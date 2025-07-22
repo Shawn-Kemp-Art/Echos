56c00dd654aa1fd9b4dad2b9969e7f121d2e236f
Shawn Kemp <shawn.kemp@gmail.com>
2025-07-21 18:08:58 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -29,11 +29,10 @@ class Random{
             var iDiv = document.createElement('div');
             iDiv.id = 'render';
             document.body.appendChild(iDiv);
-    
             if (features == null){features={};}
             var genurl = window.location.href;
             var attr = JSON.stringify(features).replace(/\"/g,"'")
-            var url = 'https://shawnkempart.bubbleapps.io/api/1.1/wf/features';
+            var url = 'https://studio.shawnkemp.art/api/1.1/wf/features';
             var xhr = new XMLHttpRequest();
             xhr.open("POST", url);
             xhr.setRequestHeader("Content-Type", "application/json");
@@ -47,7 +46,7 @@ class Random{
         function sendCanvasToBubbleAPI(canvas, fileName, request) {
             return new Promise(function(resolve, reject) {
                 var base64Image = canvas.toDataURL("image/png").replace(/^data:image\/png;base64,/, "");
-                var endpointUrl = "https://shawnkempart.bubbleapps.io/api/1.1/wf/singular";
+                var endpointUrl = "https://studio.shawnkemp.art/api/1.1/wf/singular";
                 request = new URLSearchParams(window.location.search).get('request')
                 var payload = {
                     hash: $fx.hash,
@@ -86,7 +85,7 @@ class Random{
             return new Promise(function(resolve, reject) {
                 var svg = project.exportSVG({ asString: true });
                 var base64SVG = btoa(unescape(encodeURIComponent(svg)));
-                var endpointUrl = "https://shawnkempart.bubbleapps.io/api/1.1/wf/singular";
+                var endpointUrl = "https://studio.shawnkemp.art/api/1.1/wf/singular";
         
                 var payload = {
                     hash: $fx.hash,
